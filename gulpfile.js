093822e62d35451d152420765d9e0a8f03f43578
Sérgio Adriano <sergioadrianomc@gmail.com>
2024-04-15 19:35:56 -0300

    Ajustes no cabeçalho

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+//FUNÇÃO UGLYFI PARA MINIFICAÇÃO DO CÓDIGO
+function scripts(){
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 //FUNÇÃO PARA COMPILAR ARQUIVOS SASS em CSS
 function styles(){
@@ -18,8 +26,9 @@ function images(){
 }
 
 //TAREFA PADRÃO GULP
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 //FUNÇÃO DE MONITORAMENTO
 exports.watch = function(){
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
 }
